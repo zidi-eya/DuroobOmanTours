10bfd218c116d6516904966799fd9f4b7109e2ed
Eya Zidi <eya.zidi@esprit.tn>
2025-11-05 17:30:28 +0100

    add packs

--- client-front/src/router/index.js
+++ client-front/src/router/index.js
@@ -5,6 +5,10 @@ import rentcar from '../views/rentcar.vue'
 import Pack from '../views/Packs/pack.vue'
 import comingsoon from '../components/comingsoon.vue'
 import Packdetails from '../views/Packs/packDetails1.vue'
+import Packdetails2 from '../views/Packs/packDetails2.vue'
+import Packdetails3 from '../views/Packs/packDetails3.vue'
+import Packdetails4 from '../views/Packs/packDetails4.vue'
+import Packdetails5 from '../views/Packs/packDetails5.vue'
 
 //import ToursListView from '../views/ToursListView.vue'
 
@@ -38,10 +42,41 @@ const routes = [
     component: Pack,
   },
    {
-    path: '/details1',
+    path: '/Desert-OVERNIGHT-Details',
     name: 'packdetails',
     component: Packdetails,
   },
+{
+    path: '/Jbel-SAFARI-Details',
+    name: 'packdetails3',
+    component: Packdetails3,
+  },
+ {
+    path: '/Desert-SAFARI-Details',
+    name: 'Packdetails2',
+    component: Packdetails2,
+  },
+   {
+    path: '/Salalah-City-Tour-Details',
+    name: 'packdetails4',
+    component: Packdetails4,
+  },
+   
+
+
+
+  
+   {
+    path: '/West-salalah-Details',
+    name: 'packdetails5',
+    component: Packdetails5,
+  },
+
+
+
+
+
+
 ]
 
 const router = createRouter({
